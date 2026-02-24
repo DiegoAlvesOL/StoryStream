cc15716d10a7dc10ff7a96ffd9c8ded0ee073fa9
Diego Alves de Oliveira <diego@Diegos-MacBook-Air-2.local>
2026-02-24 17:21:35 +0000

    chore: initial project structure and documentation setup

---
+++ assets/js/data_service.js
@@ -0,0 +1,12 @@
+/**
+ * This service handles all data fetching operations from the JSON mock databases.
+ * It provides data to the interface_engine.js for rendering.
+ */
+
+/**
+ * @summary Fetches data from a specific source.
+ * @description This function will be implemented to retrieve information from movies and books JSON files.
+ */
+function fetchMediaData() {
+    // Implementation will follow in the next lessons
+}
---
+++ assets/js/interface_engine.js
@@ -0,0 +1,12 @@
+/**
+ * This engine handles all DOM manipulations and user interface interactions.
+ * It receives data processed by data_service.js.
+ */
+
+/**
+ * @summary Renders the list of media items to the screen.
+ * @description Takes an array of objects and injects them into the main container.
+ */
+function renderMediaList() {
+    // Implementation will follow in the next lessons
+}
